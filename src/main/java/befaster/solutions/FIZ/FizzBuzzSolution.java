6ac562220253c2f0f36755c4f3d940b36be23504
sbx_user1060 <sbx_user1060@ip-10-129-188-237.eu-west-2.compute.internal>
2018-09-12 22:06:58 +0000

    Wed Sep 12 22:06:58 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,15 +4,26 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
     	
-    	if(number%15 == 0)
+    	if(number%15 == 0 || (isDigitPresent(number,3) && isDigitPresent(number,5) ))
     		return "fizz buzz";
-    	else if(number%3 == 0)
+    	else if(number%3 == 0 || isDigitPresent(number,3))
     		return "fizz";
-    	else if(number%5 == 0)
+    	else if(number%5 == 0 || isDigitPresent(number,5))
     		return "buzz";
     	else
     		return number.toString();
 
     }
-
+    
+    private boolean isDigitPresent(Integer x, Integer digit) {
+    	while(x>0) {
+    		if(x % 10 == digit)
+    			break;
+    		x=x/10;
+    	}
+    	
+    	return x >0;
+    }
+    
+    
 }
