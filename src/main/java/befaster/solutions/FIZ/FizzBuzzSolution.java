1c6dfe5a2979f2a8f2a2de77f7ce3569f928a335
sbx_user1060 <sbx_user1060@ip-10-129-188-237.eu-west-2.compute.internal>
2018-09-12 22:12:42 +0000

    Wed Sep 12 22:12:42 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,28 +2,30 @@ package befaster.solutions.FIZ;
 
 public class FizzBuzzSolution {
 
-    public String fizzBuzz(Integer number) {
-    	
-    	if(number%15 == 0 || (isDigitPresent(number,3) && isDigitPresent(number,5) ))
-    		return "fizz buzz";
-    	else if(number%3 == 0 || isDigitPresent(number,3))
-    		return "fizz";
-    	else if(number%5 == 0 || isDigitPresent(number,5))
-    		return "buzz";
-    	else
-    		return number.toString();
+	public String fizzBuzz(Integer number) {
 
-    }
-    
-    private boolean isDigitPresent(Integer x, Integer digit) {
-    	while(x>0) {
-    		if(x % 10 == digit)
-    			break;
-    		x=x/10;
-    	}
-    	
-    	return x >0;
-    }
-    
-    
+		if (number % 15 == 0 
+				|| (isDigitPresent(number, 3) && isDigitPresent(number, 5))
+				|| (number % 3 == 0 && isDigitPresent(number, 5)) 
+				|| (number % 5 == 0 && isDigitPresent(number, 3)))
+			return "fizz buzz";
+		else if (number % 3 == 0 || isDigitPresent(number, 3))
+			return "fizz";
+		else if (number % 5 == 0 || isDigitPresent(number, 5))
+			return "buzz";
+		else
+			return number.toString();
+
+	}
+
+	private boolean isDigitPresent(Integer x, Integer digit) {
+		while (x > 0) {
+			if (x % 10 == digit)
+				break;
+			x = x / 10;
+		}
+
+		return x > 0;
+	}
+
 }
